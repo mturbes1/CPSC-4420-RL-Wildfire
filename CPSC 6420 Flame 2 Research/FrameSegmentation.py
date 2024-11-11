9c970971307ee6646da45ad082dd64db1b5d0f71
Marshall Turbes <mturbes@clemson.edu>
2024-11-11 15:40:51 -0500

    Update FrameSegmentation.py
    Added comments to frame segmentation code

--- CPSC 6420 Flame 2 Research/FrameSegmentation.py
+++ CPSC 6420 Flame 2 Research/FrameSegmentation.py
@@ -3,6 +3,7 @@ import cv2
 import os
 import shutil
 
+#Get all RGB Videos
 RGB_videos = [f'#1-7) All Video Pairs/#{i}) RGB Video {i}.MP4' for i in range(1, 8)]
 
 RGB_video_captures = []
@@ -14,6 +15,7 @@ for video in RGB_videos:
     else:
         print(f"Error opening video file: {video}")
 
+#Get all IR Videos
 IR_videos = [f'#1-7) All Video Pairs/#{i}) IR Video {i}.MP4' for i in range(1, 8)]
 
 IR_video_captures = []
@@ -25,6 +27,7 @@ for video in IR_videos:
     else:
         print(f"Error opening video file: {video}")
 
+#Create directories for the frame pairs
 output_dir_RGB = "Segmented Frames/RGB"
 output_dir_IR = "Segmented Frames/IR"
 
@@ -47,6 +50,7 @@ while not all(finished_videos):
         IR_ret, IR_frame = IR_capture.read()
 
         if RGB_ret and IR_ret:
+            #Resize frames by half to save space
             RGB_frame = cv2.resize(RGB_frame, (0, 0), fx=0.5, fy=0.5)
             IR_frame = cv2.resize(IR_frame, (0, 0), fx=0.5, fy=0.5)
 
@@ -55,16 +59,19 @@ while not all(finished_videos):
             os.makedirs(RGB_video_dir, exist_ok=True)
             os.makedirs(IR_video_dir, exist_ok=True)
 
+            #After every 180 frames
             if frame_count % 180 == 0:
                 RGB_filename = os.path.join(RGB_video_dir, f"RGB_frame_{frame_count}.jpg")
                 IR_filename = os.path.join(IR_video_dir, f"IR_frame_{frame_count}.jpg")
                 
+                #Write frames to directory
                 cv2.imwrite(RGB_filename, RGB_frame)
                 cv2.imwrite(IR_filename, IR_frame)
             
             frame_count += 1
             
         else:
+            #When video is finished
             finished_videos[video_index] = True
             if not RGB_ret:
                 print(f"End of RGB video {video_index + 1} reached.")
